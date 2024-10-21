8cc97ffc33ad188e033f1500252d47092020f190
AndyJiang <17538006@qq.com>
2024-10-21 16:01:01 +0800

    +imgui

--- core/VeryEmulator.cpp
+++ core/VeryEmulator.cpp
@@ -1,6 +1,9 @@
 #include "SDL.h"
 #include "SDL_main.h"
 #include <glad/glad.h>
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
 
 bool quitting = false;
 void PollEvents()
@@ -8,6 +11,7 @@ void PollEvents()
 	SDL_Event event;
 	while ( SDL_PollEvent( &event ) )
 	{
+        ImGui_ImplSDL2_ProcessEvent(&event);
 		switch ( event.type )
 		{
 			case SDL_QUIT:
@@ -53,29 +57,84 @@ SDLMAIN_DECLSPEC int SDL_main(int argc, char *argv[])
 		return false;
 	}
 
+    SDL_GL_MakeCurrent(m_window, m_glContext);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+
 	if ( !gladLoadGLLoader( SDL_GL_GetProcAddress ) )
 	{
 		// LogError( "Failed to initialize OpenGL context" );
 		return false;
 	}
 
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(m_window, m_glContext);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
 	// Log( "GL_VENDOR:   %s", glGetString( GL_VENDOR ) );
 	// Log( "GL_RENDERER: %s", glGetString( GL_RENDERER ) );
 	// Log( "GL_VERSION:  %s", glGetString( GL_VERSION ) );
 
-	// clear default framebuffer
-	glClearColor( 0, 0, 0, 1 );
-	glViewport( 0, 0, winWidth, winHeight );
-	glClear( GL_COLOR_BUFFER_BIT );
+	bool show_demo_window;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     while(!quitting) {
         PollEvents();
 
-       	glClearColor( 1, 0, 0, 1 );
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+		// clear default framebuffer
+        ImGui::Render();
+       	glClearColor( clear_color.x, clear_color.y, clear_color.z, clear_color.w );
         glViewport( 0, 0, winWidth, winHeight );
         glClear( GL_COLOR_BUFFER_BIT );
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
       	SDL_GL_SwapWindow( m_window );
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(m_glContext);
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
 
     return 0;
 }
